7079cd1ecc66f2a78067e4cb7c1ccd7926b97121
Faruk <farukmirojevic185@gmail.com>
2026-08-23 23:59:43 +0200

    Popravak za render

--- backend/src/main/java/com/faruk/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/faruk/backend/config/SecurityConfig.java
@@ -88,7 +88,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:4200",
-                "https://kupi-prodaj-ecommerce-1.onrender.com"
+                "https://kupi-prodaj-ecommerce-1.onrender.com",
+                "https://kupi-prodaj-ecommiere-1.onrender.com"
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
