4d8e2b3ec38ec1dc40e1798d912ffa4a56a30320
Faruk <farukmirojevic185@gmail.com>
2026-08-25 01:28:09 +0200

    Popravio url dinamički samo u enviorment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
-export const environment={
-    production: true,
-    apiUrl: 'https://kupi-prodaj-ecommerece.onrender.com/api/v1'
-}
-
+export const environment = {
+  production: window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1',
+  apiUrl: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
+    ? 'http://localhost:8080/api/v1'
+    : 'https://kupi-prodaj-ecommerece.onrender.com/api/v1'
+};
