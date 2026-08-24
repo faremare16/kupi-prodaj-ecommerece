1a491072a6bc30a6d29dc9029d54f64039f05fc7
Faruk <farukmirojevic185@gmail.com>
2026-08-25 01:01:35 +0200

    Fix

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment={
-    production: true,
-    apiUrl: 'https://kupi-prodaj-ecommerece.onrender.com/api/v1'
-}
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment={
-    production: false,
-    apiUrl: 'http://localhost:8080/api/v1'
+    production: true,
+    apiUrl: 'https://kupi-prodaj-ecommerece.onrender.com/api/v1'
 }
+
